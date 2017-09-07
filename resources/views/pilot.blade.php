703610c6ecd47a0405c1074d2a93a8e76fbeaadb
zangles <azuresky07@gmail.com>
2017-09-07 01:02:27 -0300

    pilot view

--- resources/views/pilot.blade.php
+++ resources/views/pilot.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>GK Girls - Tiers</title>
+    <title>GK Girls - Tiers - {{ $pilot->name }}</title>
 
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
