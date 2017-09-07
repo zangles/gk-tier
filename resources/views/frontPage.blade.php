5bddf417a03103d52d423de9c3a2def799c17c60
zangles <azuresky07@gmail.com>
2017-09-06 23:27:50 -0300

    inspinia

--- resources/views/frontPage.blade.php
+++ resources/views/frontPage.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>{{ env('APP_NAME') }}</title>
+    <title>GK Girls - Tiers</title>
 
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
