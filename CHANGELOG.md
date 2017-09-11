d897e1a8ec65af199c30f8a2b5561827c91cfcbb
zangles <azuresky07@gmail.com>
2017-09-10 23:44:34 -0300

    change log

---
+++ CHANGELOG.md
@@ -0,0 +1,24 @@
+# __Change Log__
+***
+
+### __v 0.2.2 - 10-09-2017__
+
+- Added pilot dress in admin for futures updates
+
+### __v 0.2.1 - 06-09-2017__
+
+- Added share buttons
+- Added extended info button in pilot view
+
+### __v 0.2.0 - 06-09-2017__
+
+- Inspinia theme refactor
+
+### __v 0.1.1 - 06-09-2017__
+
+- Added pilot view
+
+### __v 0.1.0 - 06-09-2017__
+
+- first version with admin
+
--- resources/views/inspinia/footer.blade.php
+++ resources/views/inspinia/footer.blade.php
@@ -1,8 +1,29 @@
-<div class="footer">
+<div class="footer fixed">
     <div class="pull-right">
-
+        v 0.2.2 <span class="label label-info" style="cursor: pointer" data-toggle="modal" data-target="#changeLogModal">Change log</span>
     </div>
     <div>
         <strong>Copyright</strong> Zangles &copy; 2017
     </div>
+
+    <div class="modal fade" id="changeLogModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body changeLogBody">
+                <?php
+                    $Parsedown = new Parsedown();
+
+                    echo $Parsedown->text(file_get_contents(app_path().'/../CHANGELOG.md'));
+                    ?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+@section('scripts')
+
+@endsection
