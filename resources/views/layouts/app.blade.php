5a4ac27fd377e38d04e6e3ff85ac96e490ce97d8
zangles <azuresky07@gmail.com>
2017-09-14 01:54:29 -0300

    correccion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,24 +59,25 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
-                        <li>
-                            <a href="{{ route('update.db') }}">
-                                UpdateDb
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('change.status') }}">
-                                App Status
-                                @if (App::isDownForMaintenance())
-                                    <span class="label label-danger">DOWN</span>
-                                @else
-                                    <span class="label label-success">UP</span>
-                                @endif
-                            </a>
-                        </li>
+
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
                         @else
+                            <li>
+                                <a href="{{ route('update.db') }}">
+                                    UpdateDb
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('change.status') }}">
+                                    App Status
+                                    @if (App::isDownForMaintenance())
+                                        <span class="label label-danger">DOWN</span>
+                                    @else
+                                        <span class="label label-success">UP</span>
+                                    @endif
+                                </a>
+                            </li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
